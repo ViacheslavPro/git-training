9ab2a437f1a543056e43cfc0a98c1e543fccf24f
middleweightchampion <vakashi.yuri@gmail.com>
2025-03-20 23:56:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-console.log('Hello!');
-console.log(54+1)
+let nothing = null,
+    number = 123,
+    bool = true,
+    str = 'Hello world!',
+    obj = {
+        name: "Slavik",
+        age: 21
+    }
+    bigNumber = 9007199254740991n
+    
+;
+console.log(nothing);
+console.log(typeof(number));
+console.log(typeof(bool));
+console.log(typeof(obj));
+console.log(typeof(bigNumber));
+console.log(typeof(undef));
+console.log(typeof(str));
 
-let holySmokes = 'bullshit';
+
